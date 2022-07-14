6e71ea046e03b74de0fd04c6d3e705d254803846
sainingo <nickmentor421@gmail.com>
2022-07-14 16:09:42 +0300

    Implement class methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,7 @@ class Like < ApplicationRecord
   # belongs_to :post
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
