66f42e6df80580d212e50a94119bd9cdf9f9cbf5
sainingo <nickmentor421@gmail.com>
2022-07-14 15:02:11 +0300

    Fix linter error

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ApplicationRecord
-    # belongs_to :user
-    # belongs_to :post
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
-
+  # belongs_to :user
+  # belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,5 @@
 class Post < ApplicationRecord
-#   belongs_to :user
-#   has_many :comments
-#   has_many :likes
-   belongs_to :author, class_name: 'User'
-   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
-   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
-
+  belongs_to :author, class_name: 'User'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 end
