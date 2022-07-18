a60c92e25357da1f5c745d3697bca7cd7dee7b6f
sainingo <nickmentor421@gmail.com>
2022-07-18 13:25:30 +0300

    Create a user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
-  resources :users
-  resources :posts
+  get "/users", to: "user#user"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
