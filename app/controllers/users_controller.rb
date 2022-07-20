b162335c3558ded94af8b78f8ac750ae493c4e8e
sainingo <nickmentor421@gmail.com>
2022-07-20 12:31:51 +0300

    Add user controller spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+  def index; end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
+  # def index
+  #   @users = User.all
+  # end
+
+  # def show
+  #   @user = User.find(params[:id])
+  # end
 end
