5e8d10c8196b8c20465fe46210286272e21a83a5
sainingo <nickmentor421@gmail.com>
2022-07-20 10:18:43 +0300

    Testing post controller

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,31 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe 'PostsController', type: :request do
-  describe 'GET /index' do
-    context 'when the page is loaded' do
-      it 'returns a 200 status code and render template for index' do
-        get '/users/1/posts'
+RSpec.describe 'Posts', type: :request do
+    describe 'GET #index' do
+      before(:each) { get user_posts_path(1) }
+      it 'should have a status code of 200 ' do
         expect(response).to have_http_status(200)
+      end
+  
+      it 'should have a template of show' do
         expect(response).to render_template(:index)
       end
-      it 'includes pagination' do
-        get '/users/1/posts'
-        expect(response.body).to include('Pagination')
+  
+      it 'should have place holder of posts#index' do
+        expect(response.body).to include('posts list')
+      end
+    end
+  
+    describe 'GET #show' do
+      user_id = 1
+      id = 1
+      before(:each) { get user_post_path user_id, id }
+  
+      it 'should have correct response status' do
+        expect(response).to have_http_status(:ok)
+      end
+  
+      it 'should render correct template for show' do
+        expect(response).to render_template(:show)
+      end
+  
+      it 'should include show for posts placeholder text' do
+        expect(response.body).to include('posts show page')
       end
     end
   end
-
-  describe 'GET /show' do
-    context 'when the page is loaded' do
-      it 'returns a 200 status code and render template for show' do
-        get '/users/1/posts', params: { id: 1 }
-        expect(response).to have_http_status(200)
-        expect(response).to render_template(:index)
-      end
-      it 'returns a 200 status code and render template for show' do
-        get '/users/1/posts', params: { id: 1 }
-        expect(response.body).to include('post')
-      end
-    end
-  end
-end
