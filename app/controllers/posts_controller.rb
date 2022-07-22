9726021a7768dc7dd4ae15b3e5a9bfe54806cbb4
sainingo <nickmentor421@gmail.com>
2022-07-22 12:08:20 +0300

    Add create & new methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,34 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new
+    render :new, locals: { post: @post }
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    add_post = @user.posts.new(post_params)
+    add_post.comments_counter = 0
+    add_post.likes_counter = 0
+    respond_to do |format|
+      format.html do
+        if add_post.save
+          flash[:success] = 'Post created successfully'
+          redirect_to user_posts_url
+        else
+          flash.now[:error] = 'Error: Post could not be created'
+          render :new, locals: { post: add_post }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:new_post).permit(:title, :text)
+  end
 end
