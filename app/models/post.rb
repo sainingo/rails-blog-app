86b0640250ee713c939fd0edf8a9f2e14f28a6bc
sainingo <nickmentor421@gmail.com>
2022-07-22 12:16:36 +0300

    Modify models likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
-  # belongs_to :author, class_name: 'User'
   belongs_to :user
-  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+  has_many :likes
+  has_many :comments
 
   after_save :update_posts_counter
 
