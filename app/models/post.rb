f54fae80e9312d3d3aeeb8cee2ef46e2aef51fdd
sainingo <nickmentor421@gmail.com>
2022-07-26 12:22:28 +0300

    Modify posts controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def return_five_comments
     comment.order('created_at Desc').limit(3)
   end
@@ -13,5 +16,9 @@ class Post < ApplicationRecord
 
   def update_post_counter
     user.increment!(:posts_counter)
+  end
+
+  def update_posts_counter_after_destroy
+    user.decrement!(:posts_counter)
   end
 end
