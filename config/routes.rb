8c85dbafb2a727d5946570a716006173462abab2
sainingo <nickmentor421@gmail.com>
2022-07-27 18:08:58 +0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :register_users
+  devise_for :users
   root to: "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       post "new_comment", to: "comments#create", on: :member
+      post "like_post", to: "likes#create", on: :member
       post "like_post", to: "likes#create", on: :member
     end
   end
+
+  resources :comments, only: [:destroy]
 end
+
