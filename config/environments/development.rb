486816bc1acb9c4390471581f552ce551e5e2355
sainingo <nickmentor421@gmail.com>
2022-07-28 14:15:14 +0300

    Modify environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,21 +53,10 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Suppress logger output for asset requests.
   config.assets.quiet = true
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-
-  # devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.debug_exception_response_format = :api
+
 end
