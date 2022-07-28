e2de54c8428e75e4c33118d5cb6bb1fe250a688b
sainingo <nickmentor421@gmail.com>
2022-07-28 16:16:01 +0300

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index], defaults: {format: :json} do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
+
   resources :comments, only: [:destroy]
  
 end
