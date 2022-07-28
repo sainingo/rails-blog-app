954380b22b3447c34d5ccadca849121de990b829
sainingo <nickmentor421@gmail.com>
2022-07-28 16:22:50 +0300

    Add jwt files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         :registerable,
+         jwt_revocation_strategy: JwtDenylist
+
   has_many :posts
   has_many :likes
   has_many :comments
+
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def return_three_posts
     posts.order('created_at Desc').limit(3)
